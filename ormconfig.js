bd1de3cba953f60a31fe122f794c7a7510c9ec1d
unknown <mauricioleft121@gmail.com>
2021-07-21 17:40:09 -0300

    make it better

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,11 @@ module.exports = {
   "type": "postgres",
   "url" : process.env.DATABASE_URL,
   "ssl": true,
+  "extra": {
+    "ssl": {
+      "rejectUnauthorized": false
+    }
+  },
   "entities": [
     "./src/models/*.ts"
   ],
