98d116ec473a2781547f0c9ad6d9a54a831ef983
unknown <mauricioleft121@gmail.com>
2021-07-21 17:47:33 -0300

    make it better

--- ormconfig.js
+++ ormconfig.js
@@ -8,10 +8,10 @@ module.exports = {
     }
   },
   "entities": [
-    "./dist/models/*.ts"
+    "./dist/models/*.js"
   ],
   "migrations": [
-    "./dist/database/migrations/*.ts"
+    "./dist/database/migrations/*.js"
   ],
   "cli": {
     "migrationsDir": "./src/database/migrations"
