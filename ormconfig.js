eff8393f80382cf64e757e686879eaef8fccb2ac
unknown <mauricioleft121@gmail.com>
2021-08-04 17:56:35 -0300

    dev

--- ormconfig.js
+++ ormconfig.js
@@ -16,6 +16,13 @@
 module.exports = {
   "type": "postgres",
   "url" : process.env.DATABASE_URL,
+  "ssl": true,
+  "extra": {
+    "ssl": {
+      "rejectUnauthorized": false
+    }
+  }
+  ,
   "entities": [
     "./dist/models/*.js"
   ],
